2c5d0bd081c65b904d0ce2496bc7dd0fb57487b8
Brandon Dusch <brandondusch@gmail.com>
2020-03-03 18:16:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(deli)
     puts "The line is currently empty."
   else
     result = "The line is currently: "
-    deli.each_with_index(1){|persome, i|
+    deli.each.with_index(1){|persome, i|
       result << "#{i}. #{person}"
     }
     puts result
